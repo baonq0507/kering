be50491d11b558ac7b10cf7db0ba2f682d6c68c1
baonq0507 <nquangbao0507@gmail.com>
2024-12-03 18:15:22 +0700

    develop

--- resources/views/mission/index.blade.php
+++ resources/views/mission/index.blade.php
@@ -145,19 +145,19 @@
                                             title: error.responseJSON.title,
                                             text: error.responseJSON.message,
                                             icon: 'success',
-                                            color: "#716add",
+                                            // color: "#716add",
                                             background: "#fff url(/images/trees.png)",
-                                            backdrop: `
-                                                rgba(0,0,123,0.4)
-                                                url("/images/nyan-cat.gif")
-                                                left top
-                                                no-repeat
-                                            `
+                                            // backdrop: `
+                                            //     rgba(0,0,123,0.4)
+                                            //     url("/images/nyan-cat.gif")
+                                            //     left top
+                                            //     no-repeat
+                                            // `
                                         });
 
                                         setTimeout(() => {
                                             $('.pyro').addClass('d-none');
-                                        }, 3000);
+                                        }, 10000);
                                         return;
                                     } else {
                                         Swal.fire({
