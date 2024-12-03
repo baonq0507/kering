7aab83d62e7135ec302b97937ff9a8f6ff4cbf81
baonq0507 <nquangbao0507@gmail.com>
2024-12-03 21:07:54 +0700

    develop

--- resources/views/mission/index.blade.php
+++ resources/views/mission/index.blade.php
@@ -147,12 +147,12 @@
                                             icon: 'success',
                                             // color: "#716add",
                                             background: "#fff url(/images/trees.png)",
-                                            // backdrop: `
-                                            //     rgba(0,0,123,0.4)
-                                            //     url("/images/nyan-cat.gif")
-                                            //     left top
-                                            //     no-repeat
-                                            // `
+                                            backdrop: `
+                                                rgba(0,0,123,0.4)
+                                                url("/images/nyan-cat.gif")
+                                                left top
+                                                no-repeat
+                                            `
                                         });
 
                                         setTimeout(() => {
